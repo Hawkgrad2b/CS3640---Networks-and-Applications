05ef29a81e07478ad8416fdb5070305d62694864
wplucas <wplucas@uiowa.edu>
2024-09-05 20:54:19 +0000

    updated structure of how the code handled errors

--- A1/run.py
+++ A1/run.py
@@ -18,18 +18,14 @@ f = open('output.txt', 'w')
 # Starts the output.txt file with hawkid and name
 f.write(f"{HAWKID}\n{NAME}\n")
 
-# Checks to make sure there is a suficient amount of arguments passed into the program
-if len(sys.argv) < 2:
-    error_message = "[Error] No input provided. Please provide a target for ping and traceroute. \n"
-    print(error_message)
-    f.write(error_message)
-    sys.exit(1)
-
-# Takes the target given from the user in the command line to, will be used for ping and traceroute
-target = sys.argv[1]
-
-# The commands that will be executed when run.py is run with a given target
-commands = [
+# This block will try to run through the array of string commands
+if len(sys.argv) == 2:
+    
+    # Takes the target given from the user in the command line to, will be used for ping and traceroute
+    target = sys.argv[1]
+        
+    # The commands that will be executed when run.py is run with a given target
+    commands = [
         ["date"],
         ["whoami"],
         ["ifconfig"],
@@ -37,8 +33,6 @@ commands = [
         ["traceroute", target, "-m","10"],
     ]
 
-# This block will try to run through the array of string commands
-try:
     for command in commands:
         f.write(f"\nCommand: {' '.join(command)}\n")
         
@@ -49,13 +43,23 @@ try:
 
         # If the current command fails to run, write the error message corresponding to current command name
         except subprocess.CalledProcessError as e:
-            error_message = f"[error] {command[0]} failed: {str(e)}\n"
+            error_message = f"[error] {command[0]} failed with the return code as: {result.stdout} {str(e)}\n"
             print(error_message)
             f.write(error_message)
 
         # Specified formatting after each command is attempted to run
         f.write("\n*****\n")
 
+# If there is not a suficient amount of arguments passed into the program, print error message
+elif len(sys.argv) == 1:
+    f.write("[Error] No IP or Website was provided as an input to the command-line\n")
+    print("[Error] Please enter an IP or Website address as an input into command-line\n")
+
+# If there are too many arguments passed into the program, print error message
+elif len(sys.argv) > 2:
+    f.write("[Error] Too many parameters were enter as an input into the command-line\n")
+    print("[Error] Too many parameters were enter as an input into the command-line\n")
+
 # Close the output.txt file and print to the terminal
-finally:
-    print("commands executed and output saved to output.txt")
+f.close()
+print("commands executed and output saved to output.txt")
