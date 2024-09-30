b544094d73a824f30a9a613a552cae512aa32cca
wplucas <wplucas@uiowa.edu>
2024-09-30 21:32:45 +0000

    Updated network_bottleneck.py with input parameters

--- A3/network_bottleneck.py
+++ A3/network_bottleneck.py
@@ -16,4 +16,30 @@ from mininet.link import TCLink # traffic control links (set bandwidth limits)
 from mininet.log import setLogLevel # for logging information
 from mininet.util import dumpNodeConnections # dump connections between nodes
 
+def Create_Network(bw_bottleneck, bw_other, sim_time):
+    #in progress (-wplucas)
+    print()
 
+
+
+
+if __name__ == "__main__":
+    # Intialize the argparse parser to take the command-line args
+    parser = argparse.ArgumentParser(description="Network Bottleneck parser")
+
+    # Define the arguments, 
+    # bandwidth (for bottleneck link and other links) and time
+    parser.add_argument('-bw_bottleneck', type=int, default=10,
+                        help='Bandwidth (Mbps) for the bottleneck link (default is 10Mbps)')
+    parser.add_argument('bw_other', type=int, default=100,
+                        help='Bandwidth (Mbps) for other links (default is 100Mbps)')
+    parser.add_argument('-time', type=int, default=10,
+                        help='Duration (seconds) of traffic simulation. (default is 10 seconds)')
+    
+    args = parser.parse_args()
+
+    if args.bw_other <= args.bw_bottleneck:
+        raise ValueError('The bandwidth for other links must be higher than the ' +
+                         'specified bottleneck bandwidth')
+    
+    Create_Network(args.bw_bottleneck, args.bw_other, args.time)
