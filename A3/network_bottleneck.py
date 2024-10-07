8d780404845852f303f668138191a93d2c593c68
wplucas <wplucas@uiowa.edu>
2024-10-07 19:00:12 +0000

    inital test run, added two .json files

--- A3/network_bottleneck.py
+++ A3/network_bottleneck.py
@@ -127,43 +127,9 @@ def run_perf_tests(net, bw_bottleneck, bw_other):
     # Save UDP result to JSON file
     udp_filename= f'output-udp-{bw_bottleneck}Mbps-{bw_other}Mbps.json'
     with open(udp_filename, 'w') as f:
-        f.write(udp_result.txt)
+        f.write(udp_result.text)
     print(f'UDP results saved to {udp_filename}')
 
-    # Initialize TCP test from h1 to h3
-    tcp_client = iperf3.Client()
-    tcp_client.bind_address = h1.IP()
-    tcp_client.port = 5001
-    tcp_client.server_hostname = h3.IP()
-    tcp_client.duration = 60
-    tcp_client.json_output = True
-
-    print(f'Starting TCP test from {h1.IP()} to {h3.IP()}')
-    tcp_result = tcp_client.run()
-
-    # Save TCP result to JSON file
-    tcp_filename= f'output-tcp-{bw_bottleneck}Mbps-{bw_other}Mbps.json'
-    with open(tcp_filename, 'w') as f:
-        f.write(tcp_result.text)
-    print(f'TCP results saved to {tcp_filename}')
-
-    # Initialize UDP test from h2 to h4
-    udp_client = iperf3.Client()
-    udp_client.bind_address = h2.IP()
-    udp_client.port = 5002
-    udp_client.server_hostname = h4.IP()
-    udp_client.duration = 60
-    udp_client.protocol = 'udp'
-    udp_client.json_output = True
-
-    print(f'Starting UDP test from {h2.IP()} to {h4.IP()}')
-    udp_result = udp_client.run()
-
-    # Save UDP result to JSON file
-    udp_filename= f'output-udp-{bw_bottleneck}Mbps-{bw_other}Mbps.json'
-    with open(udp_filename, 'w') as f:
-        f.write(udp_result.txt)
-    print(f'UDP results saved to {udp_filename}')
 
 if __name__ == "__main__":
     # Clearing mininet state between executions
